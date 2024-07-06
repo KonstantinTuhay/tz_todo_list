cbcd5c9f4d25b20dfccc3ff838b95922d114a2c0
Konstantin Tuhay <tugay-k@mail.ru>
2024-07-06 17:06:12 +0300

    add: import and add file store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tasksSlice from "./slices/slice";
+import tasksReducer from "./reducers/reduce";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    tasksSlice: tasksSlice,
+    tasksReducer: tasksReducer,
+  },
 });
