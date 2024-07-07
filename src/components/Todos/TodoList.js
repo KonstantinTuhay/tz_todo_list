988930c839dd7bcbe68ae39ffd9d5cf89a4a0eae
Konstantin Tuhay <tugay-k@mail.ru>
2024-07-07 17:55:25 +0300

    try: add API without token

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -2,9 +2,20 @@ import React from "react";
 import Todo from "./Todo";
 import styles from "./TodoList.module.css";
 import { useSelector } from "react-redux";
+import { useGetTasksQuery } from "../../api";
 
 const TodoList = ({ todos, setTodos }) => {
+  const { data: tasks, error, isLoading } = useGetTasksQuery();
   const tasks = useSelector((state) => state.tasksSlice);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <div className={styles.TodoList}>
       {tasks.length === 0 && <h2>Todo list is empty</h2>}
