d69fa550f82f68bbe5c0bbd0887b23f6fe88a83b
Konstantin Tuhay <tugay-k@mail.ru>
2024-07-11 23:23:45 +0300

    add: get request

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -5,23 +5,21 @@ import { useSelector } from "react-redux";
 import { useGetToDosQuery } from "../../apiRQuery.js";
 
 const TodoList = ({ todos, setTodos }) => {
-  // const { data: tasks, error, isLoading } = useGetToDosQuery();
-  const { data } = useGetToDosQuery();
-  const tasks = data;
+  const { data: tasks, error, isLoading } = useGetToDosQuery();
+  // const { data: tasks } = useGetToDosQuery();
   console.log(tasks);
   // const tasks = useSelector((state) => state.tasksSlice);
 
-  // if (isLoading) {
-  //   return <p>Loading...</p>;
-  // }
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-  // if (error) {
-  //   return <p>Error: {error.message}</p>;
-  // }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <div className={styles.TodoList}>
-      мм
       {tasks.length === 0 && <h2>Todo list is empty</h2>}
       {tasks.map((todo) => (
         <Todo key={todo.id} todo={todo} setTodos={setTodos} todos={todos} />
